69784d1b5c5a894937971911b95f41c9d4654edc
Enna <zitasisla@gmail.com>
2026-05-19 08:33:40 +0630

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/screens/categories_screen.dart';
+
+final theme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Color.fromARGB(100, 100, 100, 100),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: const CategoriesScreen(),
+    ),
+  );
+}
